07c314e854e76c9be07952e7830790153a472e7c
Al-ameen Raji <alameenraji1@gmail.com>
2022-10-28 03:44:51 -0700

    map repos

--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 
-const FetchRepo = ({ repos, loading }) => {
-  if (loading) {
-    return <div>loading...</div>;
-  }
+const Repos = (props) => {
+  const { repos } = props;
+  // console.log("repos is: ", repos);
 
-  return (<div>
-    Hello Everyone
-  </div>);
+  const reposList =
+    repos.length !== 0 ? (
+      repos.map((repo) => 
+        <ul key={repo.id} className="w-80 p-4 text-gray-500 m-2 bg-blue-800">
+        <li className="truncate hover:text-gray-300"><a href={repo.html_url}> Repo Name: {repo.name}</a></li>
+        <li className="truncate">Language: {repo.language}</li>
+        <li className="truncate">Visibility: {repo.visibility}</li>
+        <li className="truncate">ID: {repo.id}</li>
+        </ul>
+      )
+    ) : (
+      <li>No Repos Found</li>
+    );
+
+  return (
+      <div className="flex justify-evenly flex-wrap p-4">{reposList}</div>
+
+  );
 };
 
-export default FetchRepo;
+export default Repos;
