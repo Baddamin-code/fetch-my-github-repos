09efff886b2c267216e2e66a4946ec3317d50a0a
Al-ameen Raji <alameenraji1@gmail.com>
2022-10-29 09:44:30 -0700

    update navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import Logo from "../assets/logo.png";
-import { FaBars } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
-  const handleClick = () => setNav(!nav)
+  // const [nav, setNav] = useState(false);
+  // const handleClick = () => setNav(!nav)
 
 
 
 
   return (
-    <div className="w-full h-20 flex justify-between items-center px-4 bg-gray-900 text-gray-300 [#181629]">
+    <div className="w-full h-20 flex justify-between items-center px-4 bg-gray-900 text-gray-300 mt-3">
       <div className="z-10">
         <img src={Logo} alt="Logo img" style={{ width: "50px" }} />
       </div>
       <div className="md:flex">
         <ul className="flex flex-col md:flex-row">
-          <li className="cursor-pointer px-4"><a href="">Home</a></li>
-          <li className="cursor-pointer px-4"><a href="">Error Boundary</a></li>
-          <li className="cursor-pointer px-4"><a href="">404 test</a></li>
+          <li className="px-4"><Link to="/">Home</Link></li>
+          <li className="px-4"><Link to="/repos">Repos</Link></li>
+          <li className="px-4"><Link to="/repos">404 test</Link></li>
         </ul>
       </div>
 
