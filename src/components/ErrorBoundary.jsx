cbda1f515d00b08f1e4dd991a55943d2a6cbc775
Al-ameen Raji <alameenraji1@gmail.com>
2022-10-29 10:17:07 -0700

    update style

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 class ErrorBoundary extends React.Component {
     constructor(props) {
       super(props);
@@ -15,7 +16,7 @@ class ErrorBoundary extends React.Component {
   
     render() {
       if (this.state.hasError) {
-        return <h1>Something went wrong.</h1>;
+        return <h1 className="text-gray-500 text-center text-3xl mt-40">Something went wrong. <br /> Reload page and try again. <br /> <Link to="/" className="text-white hover:text-gray-300">Go Back Home</Link></h1>;
       }
   
       return this.props.children; 
