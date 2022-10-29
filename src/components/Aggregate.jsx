876b526ce48997855493274f35671c06760b604b
Al-ameen Raji <alameenraji1@gmail.com>
2022-10-29 10:18:18 -0700

    export page not found

--- src/components/Aggregate.jsx
+++ src/components/Aggregate.jsx
@@ -2,3 +2,4 @@ export { default as Home} from "./Home";
 export { default as Navbar} from "./Navbar";
 export { default as Repos} from "./Repos";
 export { default as ErrorBoundary} from "./ErrorBoundary";
+export { default as PageNotFound} from "./PageNotFound";
